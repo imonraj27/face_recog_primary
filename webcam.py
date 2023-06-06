ceb44b0dad1c142c9f949addcb7a423d2deffde2
imonraj27 <77281501+imonraj27@users.noreply.github.com>
2023-06-06 11:04:53 +0530

    third commit

--- webcam.py
+++ webcam.py
@@ -6,7 +6,7 @@ from time import sleep
 
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
-log.basicConfig(filename='webcam.log',level=log.INFO)
+log.basicConfig(filename='webcam.log', level=log.INFO)
 
 video_capture = cv2.VideoCapture(0)
 anterior = 0
@@ -31,22 +31,23 @@ while True:
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (200, 255, 0), 3)
+        roi = frame[y:y+h, x:x+w]
+        roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+        # Replace the ROI in the original frame with the black and white version
+        frame[y:y+h, x:x+w] = cv2.cvtColor(roi_gray, cv2.COLOR_GRAY2BGR)
+
+        # Display the resulting frame
+        cv2.imshow('Video', cv2.flip(frame, 1))
 
     if anterior != len(faces):
         anterior = len(faces)
         log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
 
-
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
-
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
 
 # When everything is done, release the capture
 video_capture.release()
